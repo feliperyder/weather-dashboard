21b44967fdd5a152b1f165e7d7e12f36d0c76570
feliperyder <147994564+feliperyder@users.noreply.github.com>
2023-12-15 19:25:27 +0000

    Javascript pseudocode written

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// Variables for HTML Elements
+
+// API Key
+
+// Event Listeners
+
+// Get city name from form
+    // Validate
+    // Call function
+    // Clear form
+
+// Function to perform a fetch from OpenWeather
+    // Make request
+    // Parse the JSON
+    // Update UI
+
+// Function to Update the UI with the weather information
+    // Update the "today" section with; city name, date, weather icon, temperature, wind speed & humidity.
+    // update the "forecast" section with 5-day forecast with; date, weather icon, temperature, wind speed & humidity.
+
+// Function to handle city click from search history
+
+// Event listener? (OR GROUP AT TOP)
+
+// Function to save search cities to localStorage
+    // Check if localStorage already has search history
+        // If it does, render from localStorage 
+        // If it doesn't make API call & save to localStorage & call createSearchButton function.
+        
+// function to createSearchButton
+
+// Function to load search history from localStorage
+function loadSearchHistory()
+    // Check if localStorage already has search history
+        // If it does, render from localStorage
+        // If it doesn't make API call & save to localStorage.
+
+// Call loadSearchHistory
