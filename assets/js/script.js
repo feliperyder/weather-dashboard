a6e63f63416901bc6752b5a298e96c2745236eb4
feliperyder <147994564+feliperyder@users.noreply.github.com>
2023-12-17 21:50:14 +0000

    Js code written, 30% complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,34 @@
 // Variables for HTML Elements
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const historyList = document.getElementById('history');
+const todaySection = document.getElementById('today');
+const forecastSection = document.getElementById('forecast');
+
+
+ // queryURL is the url we'll use to query the API
+ var queryURL = "https://api.openweathermap.org/data/2.5/forecast.json?";
+
+ // Begin building an object to contain our API call's query parameters
+ // Set the API key
+ var queryParams = { "api-key": "2927b4743fce841c29656af226a1a8c9" };
 
 // API Key
+api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={2927b4743fce841c29656af226a1a8c9}
 
 // Event Listeners
-
-// Get city name from form
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Get city name from form
+    const cityName = searchInput.value;
     // Validate
+    if (cityName.trim() !== '') {
     // Call function
+    fetchWeatherData(cityName);
     // Clear form
+    earchInput.value = '';
+}
+});
 
 // Function to perform a fetch from OpenWeather
     // Make request
@@ -16,19 +37,37 @@
 
 // Function to Update the UI with the weather information
     // Update the "today" section with; city name, date, weather icon, temperature, wind speed & humidity.
+    function updateCurrentWeather(data) {
+}
     // update the "forecast" section with 5-day forecast with; date, weather icon, temperature, wind speed & humidity.
+    function updateForecast(data) {
+    }
+
+// Function to handle city click from the search history
+    function handleCityClick(cityName) {
+    }
+    // Call the fetchWeatherData function with the selected city name
+
 
 // Function to handle city click from search history
 
 // Event listener? (OR GROUP AT TOP)
 
 // Function to save search cities to localStorage
+function saveToLocalStorage(cityName) {
+}
     // Check if localStorage already has search history
         // If it does, render from localStorage 
         // If it doesn't make API call & save to localStorage & call createSearchButton function.
         
 // function to createSearchButton
-
+function addToHistory(cityName) {
+    const historyItem = document.createElement("button");
+    historyItem.classList.add("list-group-item", "list-group-item-action", "history-item");
+    historyItem.setAttribute("data-city", cityName);
+    historyItem.textContent = cityName;
+    historyList.appendChild(historyItem);
+}
 // Function to load search history from localStorage
 function loadSearchHistory()
     // Check if localStorage already has search history
